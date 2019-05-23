aba062979c2f435d906a7f670bd678aadf02aa6b
andromedaX7 <andromeda7@126.com>
2019-05-23 21:36:11 +0800

    flutter

--- android/app/src/main/java/com/zhhl/entry_exit/tour/PhotoProvider.kt
+++ android/app/src/main/java/com/zhhl/entry_exit/tour/PhotoProvider.kt
@@ -3,7 +3,7 @@ package com.zhhl.entry_exit.tour
 import androidx.core.content.FileProvider
 
 class PhotoProvider : FileProvider() {
-    companion object{
-        const val authentication="com.zhhl.entry_exit.tour.photoProvider"
+    companion object {
+        const val authentication = "com.zhhl.entryexittour.photoProvider"
     }
 }
